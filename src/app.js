c82319e263f480cab5e33cbe7318e651a550e263
Atseupser <alturria31@gmail.com>
2022-03-21 02:20:39 -0300

    agregue la dependencia cors para poder ir haciendo el dashboard

--- src/app.js
+++ src/app.js
@@ -7,12 +7,13 @@ const usersRouter = require('./routes/users');
 const apiRouter =require('./routes/apiRoutes')
 const methodOverride = require("method-override")
 const session =require ('express-session');
-const usuarioLogueado = require('./middleware/usuarioLogueado')
+const usuarioLogueado = require('./middleware/usuarioLogueado');
+const cors = require('cors');
 
 app.use(express.static(path.resolve(__dirname, '../public')))
 
-app.listen ( process.env.PORT || 3000 , () => {
-console.log('Servidor corriendo en el puerto 3000')})
+app.listen ( process.env.PORT || 3001 , () => {
+console.log('Servidor corriendo en el puerto 3001')})
 
 app.use(session({
     secret: "es un secreto",
@@ -27,7 +28,7 @@ app.use(express.json());
 
 app.use(methodOverride("_method"));
 
-
+app.use(cors());
 app.use('/products', productsRouter);
 app.use("/",mainRouter)
 app.use("/users",usersRouter)
